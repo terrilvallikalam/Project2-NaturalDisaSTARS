448fd2bb15f74af6a9c8c2d219e05701c1ed5516
jmcleary26 <jacquelyncleary@gmail.com>
2021-04-22 14:41:44 -0400

    bubble chart

---
+++ myplots.py
@@ -0,0 +1,26 @@
+import plotly.graph_objs as go
+import numpy as np
+import pandas as pd
+
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'tornado_clean.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
+
+
