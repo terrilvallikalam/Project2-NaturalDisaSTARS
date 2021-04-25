0bd6356eb4589de168fb0aed6a93f6a04e75e6d7
njneubauer <njneubauer@gmail.com>
2021-04-24 20:32:58 -0500

    added selectors.js, state_lookup.py. Updated: index.html & app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from config import sqlpassword
 import psycopg2
+from state_lookup import us_state_abbrev
 from flask import Flask, jsonify
 from flask import render_template
 
@@ -23,8 +24,6 @@ def tables():
 
 @app.route("/api/tornado_data")
 def api():
-    # f"postgresql://postgres:{sqlpassword}@localhost:5432/tornado_db"
-    # https://kb.objectrocket.com/postgresql/from-postgres-to-python-to-html-1033
     # postgres://ouvitqtn:BZiZY_67DtjmVHAZ7EtEAu5kMmDuUySX@queenie.db.elephantsql.com:5432/ouvitqtn
     db_conn = psycopg2.connect(database="ouvitqtn", user="ouvitqtn", password=f"{sqlpassword}", host="queenie.db.elephantsql.com", port="5432")
     db_cursor = db_conn.cursor()
@@ -46,7 +45,80 @@ def api():
     # render an index.html template and pass it the data you retrieved from the database
     return jsonify(records)
 
-    #print(records)
+@app.route("/api/tornado_data_years")
+def api_year():
+    # postgres://ouvitqtn:BZiZY_67DtjmVHAZ7EtEAu5kMmDuUySX@queenie.db.elephantsql.com:5432/ouvitqtn
+    db_conn = psycopg2.connect(database="ouvitqtn", user="ouvitqtn", password=f"{sqlpassword}", host="queenie.db.elephantsql.com", port="5432")
+    db_cursor = db_conn.cursor()
+    # write a statement that finds all the items in the db and sets it to a variable
+    db_cursor.execute("SELECT * FROM tornado_db")
+    tornado_table = db_cursor.fetchall()
+
+    #list of dictionaries
+    records = []
+    for row in tornado_table:
+        cols = db_cursor.description
+        record = {}
+        for i in range(len(cols)):
+            record[cols[i].name] = row[i]
+        records.append(record)
+
+    year_options = {"year":[]}
+    year_list = []
+    i = 0
+    for record in records:
+        year = record["year"]
+        if (year not in year_list):
+            year_list.append(year)
+            i += 1
+    year_list.sort()
+    year_options["year"].append(year_list)
+    db_conn.close()
+    
+    # render an index.html template and pass it the data you retrieved from the database
+    return jsonify(year_options)
+
+
+
+@app.route("/api/tornado_data_state")
+def api_state():
+    # postgres://ouvitqtn:BZiZY_67DtjmVHAZ7EtEAu5kMmDuUySX@queenie.db.elephantsql.com:5432/ouvitqtn
+    db_conn = psycopg2.connect(database="ouvitqtn", user="ouvitqtn", password=f"{sqlpassword}", host="queenie.db.elephantsql.com", port="5432")
+    db_cursor = db_conn.cursor()
+    # write a statement that finds all the items in the db and sets it to a variable
+    db_cursor.execute("SELECT * FROM tornado_db")
+    tornado_table = db_cursor.fetchall()
+
+    #list of dictionaries
+    records = []
+    for row in tornado_table:
+        cols = db_cursor.description
+        record = {}
+        for i in range(len(cols)):
+            record[cols[i].name] = row[i]
+        records.append(record)
+
+    state_options = {"state":[]}
+    states = []
+    for record in records:
+        state = record["state"]
+
+        if (state not in states):
+            states.append(state)
+            states.sort()
+    state_options["state"].append(states)
+    
+    # https://gist.github.com/rogerallen/1583593
+    abbrev_us_state = dict(map(reversed, us_state_abbrev.items()))
+    state_names_dict = {"state_name":[]}
+    for abbr in states:
+        state_names_dict["state_name"].append(abbrev_us_state[abbr])
+
+    db_conn.close()
+
+    # render an index.html template and pass it the data you retrieved from the database
+    return jsonify(state_names_dict)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/selectors.js
@@ -0,0 +1,40 @@
+d3.json("http://127.0.0.1:5000/api/tornado_data_years").then(function(year){
+    // console.log(year)
+    selectYear = d3.select("select.sel-year")
+    selectYear.selectAll("option")
+        .data(year.year[0])
+        .enter()
+        .append("option")
+        .text(function(d) {
+            console.log(d);
+            return d;
+        })
+});
+
+d3.json("http://127.0.0.1:5000/api/tornado_data_state").then(function(state){
+    // console.log(state)
+    var selectState = d3.select("select.sel-state");
+    selectState.selectAll("option")
+        .data(state['state_name'])
+        .enter()
+        .append("option")
+        .text(function(d) {
+            return d;
+        })
+});
+
+$(document).ready(function() {
+    $(".sel-year").select2({
+    placeholder: "Select Year",
+    allowClear: true,
+    width: 'resolve'
+    });
+});
+
+$(document).ready(function() {
+    $(".sel-state").select2({
+    placeholder: "Select State",
+    allowClear: true,
+    width: 'resolve'
+    });
+});
